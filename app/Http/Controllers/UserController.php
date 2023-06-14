050c867fd7b2247bde5ed338343a5811ea1cc9fe
Mohir <mohirkarimov4181@gmail.com>
2023-06-14 12:47:30 +0500

    view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,13 @@ class UserController extends Controller
     }
 
     public function show($user){
-        return 'tanlangan user: ' . $user;
+
+        $user +=1000;
+        //return 'tanlangan user: ' . $user;
+        //return view('users.show ' , ['name' => 'Mohir' , 'id' => $user ] );
+        // yoki bu pastdigi
+        return view('users.show')->with('name' ,'Mohir')
+        ->with('id' ,  $user );
     }
 
     public function create(){
